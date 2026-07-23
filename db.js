fbd73c7f8fcf6d87869b46fed60a5cbaea950745
Yunus Khan <yunuskhan@Yunuss-MacBook-Air.local>
2026-07-24 00:57:13 +0530

    Setup Neon DB

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "yunuskhan",
-    host: "localhost",
-    database: "cms_db",
-    password: "12345678",
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
